50a3a51936caacd55410282316f1b632451f3586
gabyto19 <gabuniagoga19@gmail.com>
2024-04-14 19:05:01 +0400

    singup/in done

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.css'
+  styleUrl: './sign-in.component.css',
 })
 export class SignInComponent {
-
+  toggler = true;
+  log() {
+    console.log("log==="+this.toggler);
+  }
 }
