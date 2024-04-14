df88d1526ebc983fc43f07551955bf8ec25e1252
gabyto19 <gabuniagoga19@gmail.com>
2024-04-14 18:22:53 +0400

    Sign in done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { BuildupComponent } from './buildup/buildup.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ContactComponent } from './contact/contact.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
   { path: 'buildup', component: BuildupComponent },
-  { path: 'contact', component: ContactComponent}
+  { path: 'contact', component: ContactComponent},
+  {path: 'sign-in', component: SignInComponent}
 ]
 
 @NgModule({
